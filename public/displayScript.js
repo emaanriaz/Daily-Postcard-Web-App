dfb18c506c7bceda3ea6d1da35b16a3fffd1905e
Glitch (daily-postcard-app) <none>
2021-04-06 21:36:15 +0000

    update

--- public/displayScript.js
+++ public/displayScript.js
@@ -1,31 +1,29 @@
-"use strict";
+// This code runs as soon as the page is loaded, when 
+// the script tag in the HTML file is executed. 
 
-getInfoFromServer();
+let x = document.URL;
+let url = "/getdb?" + x.split("?")[1];  // splits url on ? and gets queryID
+let xhr = new XMLHttpRequest();
+xhr.open("GET", url);
 
-function getInfoFromServer() {
-  let url = "postcardData";
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  // event listener for when http response is loaded
-  xhr.addEventListener("load", function() {
-    if (xhr.status == 200) {
-      // success
-      
-      let jsonData = JSON.parse(xhr.responseText);
-      let text = document.getElementById("editWriting");
-      let postcard = document.getElementById("postcard");
-      let im = document.getElementById("serverImage");
-      
-      text.style.fontFamily = jsonData.font;
-      text.textContent = jsonData.message;
-      postcard.style.backgroundColor = jsonData.color;
-      im.src = jsonData.image;
-      console.log("good");
-    }
-    else{
-      console.log(xhr.responseText);
-    }
-  });
-  xhr.send();
+xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+// set up callback function that will run when the HTTP response comes back
+xhr.onloadend = function(e) {
+  console.log(xhr.responseText);
+  let data = JSON.parse(xhr.responseText);
+  let postcardImage = document.getElementById("cardImg");
+  let postcardMessage = document.getElementById("message");
+  
+  postcardImage.style.display = 'block';
+  postcardImage.src = data.image;
+  postcardMessage.innerText = data.message; 
+  postcardMessage.className = data.font;
+  document.querySelector(".postcard").style.backgroundColor = data.color;
+  
 }
+
+// send off request
+xhr.send();
+
 
